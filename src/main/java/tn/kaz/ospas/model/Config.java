de2bf053809c605cc22204d6e9d4d89d3522a316
AntonKtrue <kaa-work@yandex.ru>
2017-01-29 21:19:34 +0300

    documents generator paused todo - list is not possible to generate as list - only flat text

--- src/main/java/tn/kaz/ospas/model/Config.java
+++ src/main/java/tn/kaz/ospas/model/Config.java
@@ -7,4 +7,9 @@ public class Config {
     public static final String JPA_UNIT = "appVaadinUnit";
     public static final String PARENT_FIELD = "parent";
     public static final String DOC_DIR = "E:/TEMP/";
+    public static final Double TWIPS_IN_CM = 566.929133858;
+    public static Long cmToTwips(double cm) {
+        return new Double(cm * TWIPS_IN_CM).longValue();
+    }
+
 }
